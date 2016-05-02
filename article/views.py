17b46a5677d78878010d49b4e3a2fc6cebef9dd9
Vitaly <goodgame1945@gmail.com>
2016-05-02 13:28:56 +0300

    Adding increasing likes to the articles

--- article/views.py
+++ article/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, Http404
 from django.template.loader import get_template
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
+from django.core.exceptions import ObjectDoesNotExist
 from article.models import Article, Comments
+from forms import CommentForm
+from django.core.context_processors import csrf
 
 def basic_one(request):
     view = "basic_one"
@@ -19,9 +21,20 @@ def template_three_simple(request):
     view = "template three simple"
     return render_to_response('myview.html', {'name': view})
 
-def articles(request):
-    return render_to_response('articles.html', {'articles': Article.objects.all()})
+def articles(request, article_id=1):
+    comment_form = CommentForm
+    args = {}
+    args.update(csrf(request))
+    args['article'] = Article.objects.get(id=article_id)
+    args['comments'] = Comments.objects.filter(comments_article_id=article_id)
+    args['form'] = comment_form
+    return render_to_response('article.html', args)
 
-def article(request, article_id=1):
-    return render_to_response('article.html', {'article': Article.objects.get(id=article_id),
-                                               'comments': Comments.objects.filter(comments_article_id=article_id)})
+def addlike(request, article_id):
+    try:
+        article = Article.objects.get(id=article_id)
+        article.article_likes += 1
+        article.save()
+    except ObjectDoesNotExist:
+        raise Http404
+    return redirect('/')
